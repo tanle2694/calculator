ee35cac23aacc82b45eab8845af6cde62224f31d
tanlm <tanle2694@gmail.com>
2020-09-18 23:08:01 +0700

    change

--- calculator.py
+++ calculator.py
@@ -1,5 +1,10 @@
 def add(first_term, second_term):
     return first_term + second_term
 
+
 def subtract(first_term, second_term):
     return first_term - second_term
+
+
+def multiply(first_term, second_term):
+    return first_term * second_term
